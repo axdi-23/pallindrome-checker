1ac66bba6ab2f0014193c7416aeda45ce7b7a979
dshxh <ds8354@srmist.edu.in>
2026-02-17 10:29:43 +0530

    feat: complete uc3

--- PallindromeChecker/src/PallindromeCheckerApp.java
+++ PallindromeChecker/src/PallindromeCheckerApp.java
@@ -1,5 +1,15 @@
 public class PallindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Pallindrome Checker App");
+        String ip = "madam";
+        String r_ip = "";
+        for(int i=ip.length()-1; i>=0; i--) {
+            r_ip += String.valueOf(ip.charAt(i));
+        }
+
+        if(ip.equals(r_ip)) {
+            System.out.println("ip string is pallindrome");
+        } else {
+            System.out.println("ip string is not a pallindrome");
+        }
     }
 }
